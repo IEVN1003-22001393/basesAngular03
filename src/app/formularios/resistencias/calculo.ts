eca215963cbcf26cbc00bb6abc568f433de07f82
IEVN1003-22001393 <0831alejandro@gmail.com>
2025-10-01 18:08:14 -0600

    Resistencias 01/10/25

--- src/app/formularios/resistencias/calculo.ts
+++ src/app/formularios/resistencias/calculo.ts
@@ -1,21 +1,20 @@
-
-export function calcular(primera: number, segunda: number, multiplicador: number, tolerancia: number) {
-
-
-  const valorBase = Number(`${primera}${segunda}`);
-  
-
-  const valorCalculado = valorBase * multiplicador;
-  
-
-  const margen = valorCalculado * tolerancia;
-
-
-  const resultados = {
-    valorOhm: `${valorCalculado.toLocaleString()} Ohms`,
-    valorMaximo: `${(valorCalculado + margen).toLocaleString()} Ohms`,
-    valorMinimo: `${(valorCalculado - margen).toLocaleString()} Ohms`
-  };
-
-  return resultados;
+export class calculo {
+  primera: number = 0;
+  segunda: number = 0;
+  multiplicador: number = 0;
+  tolerancia: number = 0;
+ 
+  valor: string = '';
+  valorMaximo: string = '';
+  valorMinimo: string = '';
+ 
+  obtenerResultados(): void {
+    const primeros2 = Number(`${this.primera}${this.segunda}`);
+    const multiVal =  primeros2 * this.multiplicador;
+    const valPorcent = multiVal * this.tolerancia;
+ 
+    this.valor = `${multiVal} ohms`;
+    this.valorMaximo = `${(multiVal + valPorcent)} ohms`;
+    this.valorMinimo = `${(multiVal - valPorcent)} ohms`;
+  }
 }
--- src/app/formularios/resistencias/resistencias.component.ts
+++ src/app/formularios/resistencias/resistencias.component.ts
@@ -1,40 +1,91 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
-import { calcular } from './calculo'
-
+import { FormsModule } from '@angular/forms';
+import { calculo } from './calculo';
+ 
 @Component({
   selector: 'app-resistencias',
   standalone: true,
   imports: [
-    FormsModule 
+    FormsModule
   ],
   templateUrl: './resistencias.component.html',
   styleUrls: ['./resistencias.component.css']
 })
 export class ResistenciasComponent {
-
-  primera: number | null = null;
-  segunda: number | null = null;
-  multiplicador: number | null = null;
-  tolerancia: number | null = null;
-
-  valorOhm = '';
-  valorMaximo = '';
-  valorMinimo = '';
-
-
-  calcular():void {
-
-    if (this.primera === null || this.segunda === null || this.multiplicador === null || this.tolerancia === null) {
-      return;
-    }
-
-    const resultados = calcular(this.primera, this.segunda, this.multiplicador, this.tolerancia);
-
-
-    this.valorOhm = resultados.valorOhm;
-    this.valorMaximo = resultados.valorMaximo;
-    this.valorMinimo = resultados.valorMinimo;
+ 
+  primera!: number
+  segunda!: number
+  multiplicador!: number
+  tolerancia!: number
+  ohms: string = ''
+  Max: string = ''
+  Min: string = ''
+  color1: string =''
+  color2: string =''
+  colorMultiplicador: string =''
+  tol: string = ''
+  color1Class: string =''
+  color2Class: string =''
+  colorMultClass: string =''
+  tolClass: string = ''
+ 
+  color: { [key:number]: {name:string, class:string} } = {
+    0: {name: 'Negro', class: 'negro'},
+    1: {name: 'Café', class: 'cafe'},
+    2: {name: 'Rojo', class: 'rojo'},
+    3: {name: 'Naranja', class: 'naranja'},
+    4: {name: 'Amarillo', class: 'amarillo'},
+    5: {name: 'Verde', class: 'verde'},
+    6: {name: 'Azul', class: 'azul'},
+    7: {name: 'Violeta', class: 'morado'},
+    8: {name: 'Gris', class: 'gris'},
+    9: {name: 'Blanco', class: 'blanco'}
   }
-
+ 
+  multicolor: { [key:number]: {name:string, class:string} } = {
+    1: {name: 'Negro', class: 'negro'},
+    10: {name: 'Cafe', class: 'cafe'},
+    100: {name: 'Rojo', class: 'rojo'},
+    1000: {name: 'Naranja', class: 'naranja'},
+    10000: {name: 'Amarillo', class: 'amarillo'},
+    0.1: {name: 'Dorado', class: 'dorado'},
+    0.01: {name: 'Plata', class: 'plata'}
+  }
+ 
+  tolcol: { [key:number]: {name:string, class:string} } = {
+    0.05: {name: 'Dorado', class: 'dorado'},
+    0.10: {name: 'Plata', class: 'plata'}
+  }
+ 
+ 
+  calcular():void {
+ 
+    const primerCol = this.color[this.primera]
+    this.color1 = primerCol.name
+    this.color1Class = primerCol.class
+ 
+    const segundoCol = this.color[this.segunda]
+    this.color2 = segundoCol.name
+    this.color2Class = segundoCol.class
+ 
+    const multiCol = this.multicolor[this.multiplicador]
+    this.colorMultiplicador = multiCol.name
+    this.colorMultClass = multiCol.class
+ 
+    const toleCol = this.tolcol[this.tolerancia]
+    this.tol = toleCol.name
+    this.tolClass = toleCol.class
+ 
+const miCalculo = new calculo();
+ 
+    miCalculo.primera = this.primera;
+    miCalculo.segunda = this.segunda;
+    miCalculo.multiplicador = this.multiplicador;
+    miCalculo.tolerancia = this.tolerancia;
+ 
+    miCalculo.obtenerResultados();
+    this.ohms = miCalculo.valor;
+    this.Max = miCalculo.valorMaximo;
+    this.Min = miCalculo.valorMinimo;
+  }
 }
